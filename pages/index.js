6bcdcbbec3cda0e1207962cb36ca936e6f2ff559
Michelle Lamoureaux <michellelamx@macbook.local>
2023-04-03 09:34:19 -0700

    added some text

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,7 @@ export default function Home() {
         </div>
         <div className='container'>
           <h2>Hi, I'm Michelle.</h2>
+          <h3>I'm a front-end developer.</h3>
         </div>
       </main>
     </>
