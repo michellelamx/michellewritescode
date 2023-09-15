b477ab5abd042c181264617530d34efe2d23ed31
Michelle Lamoureaux <michellelamx@macbook.local>
2023-09-15 14:59:43 -0700

    updated subheader text

---
+++ components/Skills.jsx
@@ -0,0 +1,101 @@
+export function Skills() {
+
+  return (
+    <div className='skills-wrapper'>
+      <div className='skills'>
+        <h2>Skills</h2>
+        <ul>
+          <li>
+            React &amp; JavaScript
+            <ul>
+              <li>Vite</li>
+              <li>Next.js</li>
+              <li>Create React App</li>
+            </ul>
+          </li>
+          <li>React hooks &amp; state management
+            <ul>
+              <li>Redux</li>
+              <li>Context</li>
+              <li>Zustand</li>
+            </ul>
+          </li>
+          <li>CSS
+            <ul>
+              <li>Grid &amp; Flexbox</li>
+              <li>SCSS/LESS</li>
+              <li>PostCSS</li>
+              <li>CSS Modules</li>
+              <li>design tokens</li>
+            </ul>
+          </li>
+          <li>API integration
+            <ul>
+              <li>Fetch</li>
+              <li>Axios</li>
+              <li>GraphQL</li>
+            </ul>
+          </li>
+          <li>testing &amp; debugging</li>
+          <li>Node</li>
+          <li>Docker</li>
+        </ul>
+      </div>
+      <div className='tools'>
+        <h2>Tools &amp; Libraries</h2>
+        <ul>
+          <li>task managers &amp; bundlers
+            <ul>
+              <li>Webpack</li>
+              <li>Gulp</li>
+              <li>Grunt</li>
+            </ul>
+          </li>
+          <li>version control &amp; repositories
+            <ul>
+              <li>GitLab</li>
+              <li>GitHub</li>
+              <li>Bitbucket</li>
+              <li>CI/CD pipelines</li>
+            </ul>
+          </li>
+          <li>code linting
+            <ul>
+              <li>Stylelint</li>
+              <li>ESLint</li>
+            </ul>
+          </li>
+          <li>UI component libraries</li>
+          <li>command line</li>
+          <li>npm</li>
+          <li>
+            performance optimization
+            <ul>
+              <li>lazy loading &amp; code splitting</li>
+              <li>caching strategies</li>
+              <li>server-side rendering</li>
+              <li>bundling &amp; minification</li>
+              <li>browser DevTools</li>
+              <li>React Profiler</li>
+              <li>performance tools
+                <ul>
+                  <li>Web Vitals</li>
+                  <li>webhint</li>
+                  <li>PageSpeed Insights</li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          <li>design-related
+            <ul>
+              <li>Figma</li>
+              <li>Sketch</li>
+              <li>Photoshop</li>
+              <li>design systems</li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { Skills } from '../components/Skills'
 
 export default function Home() {
   return (
@@ -13,9 +14,9 @@ export default function Home() {
           <header>
             <p className='code-font'>Hey there! My name is...</p>
             <p className='name'>Michelle Lamoureaux</p>
-            <p className='subtitle'>I'm a frontend/UI developer focused on making designs come to life with pixel-perfect, clean, reusable code.</p>
+            <p className='subtitle'>I'm a frontend developer making designs come to life with pixel-perfect, clean, performant, reusable code.</p>
           </header>
-          <footer />
+          <Skills />
         </div>
       </main>
     </>
