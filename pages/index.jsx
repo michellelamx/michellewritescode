65f33b2ada16ffe25d6bcb302db074c70d96b7ff
Michelle Lamoureaux <michellelamx@macbook.local>
2023-09-26 17:25:36 -0700

    updated content: links and email

--- pages/index.jsx
+++ pages/index.jsx
@@ -16,6 +16,18 @@ export default function Home() {
             <p className='name'>Michelle Lamoureaux</p>
             <p className='subtitle'>I'm a frontend developer making designs come to life with pixel-perfect, clean, performant, reusable code.</p>
           </header>
+          <div className='top-row'>
+            <div className='links'>
+              <h2>Links</h2>
+              <a href='https://github.com/michellelamx?tab=repositories' target='_blank'>GitHub</a>
+              <a href='https://www.linkedin.com/in/michellelamoureaux/' target='_blank'>LinkedIn</a>
+              <a href='/MichelleLamoureaux-resume-FrontEndDeveloper.pdf' download>resume</a>
+            </div>
+            <div className='contact'>
+              <h2>Get in Touch</h2>
+              <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#109;&#105;&#99;&#104;&#101;&#108;&#108;&#101;&#46;&#108;&#97;&#109;&#111;&#117;&#114;&#101;&#97;&#117;&#120;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;">&#109;&#105;&#99;&#104;&#101;&#108;&#108;&#101;&#46;&#108;&#97;&#109;&#111;&#117;&#114;&#101;&#97;&#117;&#120;&#64;&#103;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;</a>
+            </div>
+          </div>
           <Skills />
         </div>
       </main>
